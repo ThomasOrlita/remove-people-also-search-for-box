d041c5e5a98c4670ca79fbe543dfcc06d3d7e180
codebymiles <54468198+codebymiles@users.noreply.github.com>
2022-12-13 17:55:12 +1100

    Update contentScript.js to remove boxes id: aob
    Update contentScript.js to remove boxes starting with aob as well, since Google has begun using these instead.

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,5 @@
 // removes the "people also search for" box
-document.querySelectorAll('div[id^=eob]').forEach(e => {
+document.querySelectorAll('div[id^=eob], div[id^=aob]').forEach(e => {
     const parentElement = e.parentElement;
     e.remove();
     parentElement.style.height = 'auto';
